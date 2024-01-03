fe978d20a82f3650c5296a79e7c61bf2155d5e21
justadude12 <72972938+justadude12@users.noreply.github.com>
2024-01-03 13:30:23 +0100

    Oiseau vole

--- Flappy-Bird/build/bird.h
+++ Flappy-Bird/build/bird.h
@@ -4,12 +4,12 @@
 //======================================================================
 //
 //	bird, 32x32@8, 
-//	Transparent color : 95,F9,CB
+//	Transparent color : 00,FF,00
 //	+ palette 256 entries, not compressed
 //	+ 16 tiles not compressed
 //	Total size: 512 + 1024 = 1536
 //
-//	Time-stamp: 2024-01-03, 12:02:23
+//	Time-stamp: 2024-01-03, 13:26:01
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
